f5a8f05904c5158fb372c7fedd2429910b1d0dd4
florzoye <144818335+florzoye@users.noreply.github.com>
2025-11-13 17:02:07 +0700

    states: add more state for bot

--- src/bot/states.py
+++ src/bot/states.py
@@ -1,12 +1,26 @@
-from aiogram.fsm.state import StatesGroup, State
+from aiogram.fsm.state import State, StatesGroup
 
 class RegisterState(StatesGroup):
     waiting_for_address = State()
+    waiting_for_private_key = State()
     reset_address = State()
+    reset_private_key = State()
+    waiting_for_api_key = State()
+    waiting_for_api_secret = State()
+    waiting_for_api_passphrase  = State()
+
 
 class TrackSettings(StatesGroup):
     waiting_for_new_wallet = State()
     waiting_for_delete_wallet = State()
-    waiting_for_count = State()
-    waiting_for_min_value = State()
-    configuring_display = State()
+
+class CopyTradeState(StatesGroup):
+    selecting_wallet = State()
+    setting_duration = State()
+    setting_min_amount = State()
+    setting_first_bet = State()
+    setting_min_quote = State()
+    setting_max_quote = State()
+    setting_margin = State()  
+    confirming_settings = State()
+    monitoring = State()
