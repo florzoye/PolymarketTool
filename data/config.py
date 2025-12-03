aaec726d6be7398a6d8fa4bf554ecd714d4d94b9
florzoye <florzoye.official@gmail.com>
2025-12-03 13:24:47 +0700

    add pyproject and uv.lock

--- data/config.py
+++ data/config.py
@@ -1,7 +1,7 @@
 import os
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 
-load_dotenv()
+load_dotenv(find_dotenv())
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 ATTEMPS = 3
