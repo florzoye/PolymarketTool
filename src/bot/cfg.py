0a43e790cd6fd9ec7e1ef58efd363e8bff311021
florzoye <florzoye.official@gmail.com>
2025-12-06 21:02:26 +0700

    MAIN FUNC FOR SETUP BOT AND DB

--- src/bot/cfg.py
+++ src/bot/cfg.py
@@ -1,20 +1,17 @@
 import asyncio
+import logging
 from typing import Dict
-from data.config import Config
 
 from aiogram import Bot, Dispatcher
 from aiogram.types import BotCommand
 
-from db.sqlite.users import UsersSQL
-from db.sqlite.manager import AsyncDatabaseManager
+from data.config import Config
+from db.database import database
 
+logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=Config.BOT_TOKEN)
 dp = Dispatcher()
-
-db = AsyncDatabaseManager('data/users.db')
-users_sql = UsersSQL(db)
-
 active_monitors: Dict[int, asyncio.Task] = {}
 
 
@@ -25,3 +22,28 @@ async def set_commands(bot: Bot):
         BotCommand(command="copy_trade", description="Отслеживать и повторять новые сделки кошельков"),
     ]
     await bot.set_my_commands(commands)
+
+
+async def main():
+    try:
+        await database.setup()
+        
+        await set_commands(bot)
+        
+        from src.bot.handlers import start, positions, leaderboard, copy_trade, charts
+        
+        dp.include_router(start.router)
+        dp.include_router(positions.router)
+        dp.include_router(leaderboard.router)
+        dp.include_router(copy_trade.router)
+        dp.include_router(charts.router)
+        
+        print("🚀 Бот запущен")
+        await dp.start_polling(bot)
+        
+    except Exception as e:
+        logging.exception("❌ Критическая ошибка в боте:")
+        
+    finally:
+        await bot.session.close()
+        await database.close()
