ce8bca7d9fe546afd20b10f25a7d4fc85a2ae611
florzoye <florzoye.official@gmail.com>
2025-12-06 11:45:26 +0700

    add new fir for PostgreSQL

---
+++ db/sqlalchemy/models.py
@@ -0,0 +1,22 @@
+from typing import Annotated
+from sqlalchemy import Integer, JSON, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+strnullable = Annotated[str | None, mapped_column(String, nullable=True)]
+
+class Base(DeclarativeBase):
+    def __repr__(self):
+        col = [col for col in self.__table__.columns.keys()]
+        return f'{self.__class__.__name__}(' + ', '.join(f'{c}={getattr(self, c)!r}' for c in col) + ')'
+
+
+class Users(Base):
+    __tablename__ = "users"
+
+    tg_id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    track_addresses: Mapped[list[str] | None] = mapped_column(JSON, nullable=True)
+    address: Mapped[strnullable] 
+    private_key: Mapped[strnullable] 
+    api_key: Mapped[strnullable] 
+    api_secret: Mapped[strnullable] 
+    api_passphrase: Mapped[strnullable] 
